b61cfc1c7e45da61971599b725fe85e9771557a2
forcespace <force.space@rambler.ru>
2021-12-12 21:47:29 +0300

    .

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include <filesystem>
+#include <sstream>
 #include "src/map.h"
 #include "src/view.h"
 
 constexpr unsigned WINDOW_WIDTH = 1920;
 constexpr unsigned WINDOW_HEIGHT = 1080;
 constexpr unsigned CAM_WIDTH = 1200;
-constexpr unsigned CAM_HEIGHT = 678;
+constexpr unsigned CAM_HEIGHT = 674;
 
 class Player
 {
@@ -15,7 +15,8 @@ private:
     float x, y = 0;
 public:
     float w, h, dx{}, dy{}, speed = 0;
-    int dir = 0;
+    int dir, playerScore, health = 0;
+    bool life;
     sf::String File;
     sf::Image image;
     sf::Texture texture;
@@ -23,6 +24,10 @@ public:
 
     Player(const sf::String &F, int X, int Y, float W, float H)
     {
+        dir = 0;
+        playerScore = 0;
+        health = 100;
+        life = true;
         File = F;
         w = W;
         h = H;
@@ -62,6 +67,12 @@ public:
 
         speed = 0;
         sprite.setPosition(x, y);
+        interactionWithMap();
+
+        if (health <= 0)
+        {
+            life = false;
+        }
     }
 
     float getPlayerCoordinateX()
@@ -73,12 +84,64 @@ public:
     {
         return y;
     }
+
+    void interactionWithMap()
+    {
+        for (int i = y / 32; i < (y + h) / 32; i++)
+            for (int j = x / 32; j < (x + w) / 32; j++)
+            {
+                if (TileMap[i][j] == '0')
+                {
+                    if (dy > 0)
+                    {
+                        y = i * 32 - h;
+                    }
+                    if (dy < 0)
+                    {
+                        y = i * 32 + 32;
+                    }
+                    if (dx > 0)
+                    {
+                        x = j * 32 - w;
+                    }
+                    if (dx < 0)
+                    {
+                        x = j * 32 + 32;
+                    }
+                }
+
+                if (TileMap[i][j] == 's')
+                {
+                    playerScore++;
+                    TileMap[i][j] = ' ';
+                }
+
+                if (TileMap[i][j] == 'f')
+                {
+                    health -= 50;
+                    TileMap[i][j] = ' ';
+                }
+
+                if (TileMap[i][j] == 'h')
+                {
+                    health += 10;
+                    TileMap[i][j] = ' ';
+                }
+            }
+    }
 };
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML Application");
     view.reset(sf::FloatRect(0, 0, CAM_WIDTH, CAM_HEIGHT));
+
+    sf::Font font;
+    font.loadFromFile("upload/font/EuclidCircularB-Regular.ttf");
+    sf::Text healthText("", font, 20);
+    sf::Text timeText("", font, 20);
+    healthText.setColor(sf::Color::Red);
+    timeText.setColor(sf::Color::Red);
 
     sf::Image map_image;
     map_image.loadFromFile("upload/images/map.png");
@@ -91,10 +154,18 @@ int main()
 
     double currentFrame = 0;
     sf::Clock clock;
+    sf::Clock gameTimeClock;
+    int gameTime = 0;
 
     while (window.isOpen())
     {
         float time = clock.getElapsedTime().asMicroseconds();
+
+        if (dino.life)
+        {
+            gameTime = gameTimeClock.getElapsedTime().asSeconds();
+        }
+
         clock.restart();
         time = time / 800;
 
@@ -108,58 +179,56 @@ int main()
             }
         }
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        if (dino.life)
         {
-            dino.dir = 3;
-            dino.speed = 0.1;
-            currentFrame += 0.011 * time;
-            if (currentFrame > 4)
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
             {
-                currentFrame -= 2;
+                dino.dir = 3;
+                dino.speed = 0.1;
+                currentFrame += 0.011 * time;
+                if (currentFrame > 4)
+                {
+                    currentFrame -= 2;
+                }
+                dino.sprite.setTextureRect(sf::IntRect(88 * int(currentFrame), 0, 88, 94));
             }
-            dino.sprite.setTextureRect(sf::IntRect(88 * int(currentFrame), 0, 88, 94));
-            getPlayerCoordinateForView(dino.getPlayerCoordinateX(), dino.getPlayerCoordinateY());
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {
-            dino.dir = 0;
-            dino.speed = 0.1;
-            currentFrame += 0.011 * time;
-            if (currentFrame > 4)
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
             {
-                currentFrame -= 2;
+                dino.dir = 0;
+                dino.speed = 0.1;
+                currentFrame += 0.011 * time;
+                if (currentFrame > 4)
+                {
+                    currentFrame -= 2;
+                }
+                dino.sprite.setTextureRect(sf::IntRect(88 * int(currentFrame), 0, 88, 94));
             }
-            dino.sprite.setTextureRect(sf::IntRect(88 * int(currentFrame), 0, 88, 94));
-            getPlayerCoordinateForView(dino.getPlayerCoordinateX(), dino.getPlayerCoordinateY());
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        {
-            dino.dir = 2;
-            dino.speed = 0.1;
-            currentFrame += 0.011 * time;
-            if (currentFrame > 4)
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
             {
-                currentFrame -= 2;
+                dino.dir = 2;
+                dino.speed = 0.1;
+                currentFrame += 0.011 * time;
+                if (currentFrame > 4)
+                {
+                    currentFrame -= 2;
+                }
+                dino.sprite.setTextureRect(sf::IntRect(88 * int(currentFrame), 0, 88, 94));
             }
-            dino.sprite.setTextureRect(sf::IntRect(88 * int(currentFrame), 0, 88, 94));
-            getPlayerCoordinateForView(dino.getPlayerCoordinateX(), dino.getPlayerCoordinateY());
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        {
-            dino.dir = 1;
-            dino.speed = 0.1;
-            currentFrame += 0.011 * time;
-            if (currentFrame > 4)
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
             {
-                currentFrame -= 2;
+                dino.dir = 1;
+                dino.speed = 0.1;
+                currentFrame += 0.011 * time;
+                if (currentFrame > 4)
+                {
+                    currentFrame -= 2;
+                }
+                dino.sprite.setTextureRect(sf::IntRect(88 * int(currentFrame), 0, 88, 94));
             }
-            dino.sprite.setTextureRect(sf::IntRect(88 * int(currentFrame), 0, 88, 94));
-            getPlayerCoordinateForView(dino.getPlayerCoordinateX(), dino.getPlayerCoordinateY());
         }
 
+        getPlayerCoordinateForView(dino.getPlayerCoordinateX(), dino.getPlayerCoordinateY());
         dino.update(time);
-        viewMap(time);
-        changeView();
         window.setView(view);
         window.clear();
 
@@ -178,13 +247,31 @@ int main()
                 {
                     s_map.setTextureRect(sf::IntRect(64, 0, 32, 32));
                 }
+                if (TileMap[i][j] == 'f')
+                {
+                    s_map.setTextureRect(sf::IntRect(96, 0, 32, 32));
+                }
+                if (TileMap[i][j] == 'h')
+                {
+                    s_map.setTextureRect(sf::IntRect(128, 0, 32, 32));
+                }
 
                 s_map.setPosition(j * 32, i * 32);
 
                 window.draw(s_map);
+
+                std::ostringstream playerHealthString, gameTimeString;
+                playerHealthString << dino.health;
+                gameTimeString << gameTime;
+                healthText.setString("Health: " + playerHealthString.str());
+                healthText.setPosition(view.getCenter().x + (float) CAM_WIDTH / 2 - 125, view.getCenter().y - (float) CAM_HEIGHT / 2 + 20);
+                timeText.setString("Time: " + gameTimeString.str());
+                timeText.setPosition(view.getCenter().x + (float) CAM_WIDTH - CAM_WIDTH + 125, view.getCenter().y - (float) CAM_HEIGHT / 2 + 20);
             }
 
         window.draw(dino.sprite);
+        window.draw(healthText);
+        window.draw(timeText);
         window.display();
     }
 }
--- src/view.h
+++ src/view.h
@@ -19,58 +19,20 @@ void getPlayerCoordinateForView(float x, float y)
     float tempX = x;
     float tempY = y;
 
-    if (x < 200)
-    {
-        tempX = 200;
-    }
-    else if (y < 240)
-    {
-        tempY = 240;
-    }
-    else if (y > 554)
-    {
-        tempY = 554;
-    }
+//    if (x < 57)
+//    {
+//        tempX = 57;
+//    }
+//    else if (y < 57)
+//    {
+//        tempY = 57;
+//    }
+//    else if (y > 57)
+//    {
+//        tempY = 57;
+//    }
 
     view.setCenter(tempX, tempY);
-}
-
-sf::View viewMap(float time)
-{
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-    {
-        view.move(0, -0.1 * time);
-    }
-
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-    {
-        view.move(0.1 * time, 0);
-    }
-
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-    {
-        view.move(0, 0.1 * time);
-    }
-
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-    {
-        view.move(-0.1 * time, 0);
-    }
-
-    return view;
-}
-
-sf::View changeView()
-{
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::O))
-    {
-        view.setSize(CAM_DEFAULT_WIDTH, CAM_DEFAULT_HEIGHT);
-    }
-
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::P))
-    {
-        view.setSize(CAM_NEW_WIDTH, CAM_NEW_HEIGHT);
-    }
 }
 
 #endif //PROJECT_ACTIVITIES_VIEW_H
